732e8fe38fa78625b126645f36e1212c7b856c9f
RyanKor <equus3144@gmail.com>
2019-07-27 20:21:10 +0900

    frontend login function added.

--- main/frontend/src/store.js
+++ main/frontend/src/store.js
@@ -25,6 +25,11 @@ export default new Vuex.Store({
       state.isLogin = false;
       state.isLoginError = false;
       state.userInfo = null;
+    },
+    logout(state) {
+      state.isLogin = false;
+      state.isLoginError = false;
+      state.userInfo = null;
     }
   },
   actions: {
@@ -43,10 +48,31 @@ export default new Vuex.Store({
         .post("https://reqres.in/api/login", loginObj)
         // loginObj = {email,password}
         .then(res => {
-          console.log(res);
+          // 접근 성공시, 토큰 값이 반환된다. (실제로는 토큰과 함께 유저 id를 받아온다.)
+          // 토큰을 헤더 정보에 포함시켜서 유저 정보를 요청
+          let token = res.data.token;
+          let config = {
+            headers: {
+              "access-token": token
+            }
+          };
+          axios
+            .get("https://reqres.in/api/users/2", config)
+            .then(response => {
+              let userInfo = {
+                id: response.data.data.id,
+                first_name: response.data.data.first_name,
+                last_name: response.data.data.last_name,
+                avatar: response.data.data.avatar
+              };
+              commit("loginSuccess", userInfo);
+            })
+            .catch(() => {
+              alert("이메일과 비밀번호를 확인하세요.");
+            });
         })
         .catch(err => {
-          console.log(err);
+          alert("이메일과 비밀번호를 확인하세요.");
         });
     },
     logout({ commit }) {
